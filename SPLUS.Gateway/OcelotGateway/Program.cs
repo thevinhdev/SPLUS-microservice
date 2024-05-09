f3500e232233c47a8ee3a44e0c830c718ebb56d3
thevinhdev <thevinhdev@gmail.com>
2024-05-09 16:43:24 +0700

    update

--- SPLUS.Gateway/OcelotGateway/Program.cs
+++ SPLUS.Gateway/OcelotGateway/Program.cs
@@ -13,31 +13,31 @@ internal class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+        // Config Ocelot
+        builder.Configuration.AddJsonFile("ocelot.json");
+
         #region Authen
-        //var key = Encoding.UTF8.GetBytes(builder.Configuration["AppSettings:JwtKey"]!);
-        //JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
-        //builder.Services.AddAuthentication(option =>
-        //{
-        //    option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        //    option.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-        //    option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-        //}).AddJwtBearer(options =>
-        //{
-        //    options.RequireHttpsMetadata = false;
-        //    options.SaveToken = true;
-        //    options.TokenValidationParameters = new TokenValidationParameters
-        //    {
-        //        IssuerSigningKey = new SymmetricSecurityKey(key),
-        //        ValidateIssuerSigningKey = true,
-        //        ValidateIssuer = false,
-        //        ValidateAudience = false
-        //    };
-        //});
+        var key = Encoding.UTF8.GetBytes(builder.Configuration["AppSettings:JwtKey"]!);
+        builder.Services.AddAuthentication(option =>
+        {
+            option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.RequireHttpsMetadata = false;
+            options.SaveToken = true;
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuerSigningKey = true,
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+        });
         #endregion
 
-        builder.Configuration.AddJsonFile("ocelot.json", false, false);
-
         builder.Services.AddOcelot(builder.Configuration);
+
         builder.Services.AddJwtAuthentication();
 
         var app = builder.Build();
