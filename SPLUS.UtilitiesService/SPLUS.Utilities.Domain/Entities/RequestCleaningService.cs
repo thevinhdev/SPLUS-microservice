af3e85be2f71014930a408b73649014d2dd939cf
thevinhdev <thevinhdev@gmail.com>
2024-06-11 16:47:22 +0700

    update

---
+++ SPLUS.UtilitiesService/SPLUS.Utilities.Domain/Entities/RequestCleaningService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace SPLUS.Utilities.Domain.Entities;
+
+public partial class RequestCleaningService
+{
+    [Key]
+    public int Id { get; set; }
+
+    [StringLength(20)]
+    [Unicode(false)]
+    public string ProjectCode { get; set; } = null!;
+
+    [StringLength(20)]
+    [Unicode(false)]
+    public string TowerCode { get; set; } = null!;
+
+    [StringLength(20)]
+    [Unicode(false)]
+    public string ApartmentCode { get; set; } = null!;
+
+    [StringLength(255)]
+    public string ApartmentName { get; set; } = null!;
+
+    public int ResidentId { get; set; }
+
+    [StringLength(255)]
+    public string ResidentName { get; set; } = null!;
+
+    [StringLength(255)]
+    public string UserRequestName { get; set; } = null!;
+
+    [StringLength(255)]
+    public string? UserRequestPhone { get; set; }
+
+    public int ProcessStatus { get; set; }
+
+    [StringLength(512)]
+    public string Note { get; set; } = null!;
+
+    public int CreatedById { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime CreatedAt { get; set; }
+
+    public int? UpdatedById { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? UpdatedAt { get; set; }
+}
---
+++ SPLUS.UtilitiesService/SPLUS.Utilities.Domain/Entities/UtilitiesDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace SPLUS.Utilities.Domain.Entities;
+
+public partial class UtilitiesDbContext : DbContext
+{
+    public UtilitiesDbContext(DbContextOptions<UtilitiesDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<RequestCleaningService> RequestCleaningService { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<RequestCleaningService>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__RequestC__3214EC0729EDF82C");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
